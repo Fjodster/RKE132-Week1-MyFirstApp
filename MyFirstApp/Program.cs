d34bb5d6a10f6235819a19e6e1934263e97d12c7
Fjodor Pasitsnik <fjodor.pasitsnik@tktk.ee>
2023-09-16 11:14:07 -0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); // Output
+string userName = Console.ReadLine(); // Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); // Output
+//String interpolation
+
+Console.WriteLine($"Hello, {userName} !"); // Output
